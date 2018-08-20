f910124538a193847d5004cc763d1b634a1d16c8
hechenglong <kfhechenglong@126.com>
2018-08-20 18:28:32 +0800

    design

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+// import 'element-theme-default';
+import 'antd/dist/antd.less';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/login/App-login.js
+++ src/login/App-login.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-
+import { DatePicker,Card} from 'antd'
 import './login.less';
-
+import 'antd/dist/antd.less';
 class Login extends Component {
   render() {
     return (
         <div className="login">
             <div className="warp">
                 <header className="login-l tc">
-                    <h3 className="App-title">欢迎使用后台管理系统</h3>
+                    <h3 className="App-title fz22">欢迎使用后台管理系统</h3>
+										<p><span>说明：</span> 请使用微信扫一扫登录</p>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div className="App-intro">
+                    <Card className="box-card">
+											<div className="code-qq">
+                            <iframe title="iframe" id="wxcode" name="wxcode" src="https://nx.smsc.net.cn/wxopen/app/shop/wxweblogin.php" frameBorder="0" scrolling="no" width="400px" height="400px;"></iframe>
+                        </div>
+										</Card>
+										<DatePicker />
+                </div>
             </div>
         </div>
     );
