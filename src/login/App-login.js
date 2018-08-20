7a85f1d98b0c68df6240389d4c2c896987a4529e
hechenglong <kfhechenglong@126.com>
2018-08-20 18:35:20 +0800

    login over

--- src/login/App-login.js
+++ src/login/App-login.js
@@ -7,18 +7,19 @@ class Login extends Component {
     return (
         <div className="login">
             <div className="warp">
+							<Card>
                 <header className="login-l tc">
                     <h3 className="App-title fz22">欢迎使用后台管理系统</h3>
 										<p><span>说明：</span> 请使用微信扫一扫登录</p>
                 </header>
                 <div className="App-intro">
-                    <Card className="box-card">
+                    <div className="t-c">
 											<div className="code-qq">
                             <iframe title="iframe" id="wxcode" name="wxcode" src="https://nx.smsc.net.cn/wxopen/app/shop/wxweblogin.php" frameBorder="0" scrolling="no" width="400px" height="400px;"></iframe>
                         </div>
-										</Card>
-										<DatePicker />
+										</div>
                 </div>
+								</Card>
             </div>
         </div>
     );
